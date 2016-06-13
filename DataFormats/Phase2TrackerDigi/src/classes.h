70f1e82201ad89693a191a8983a53ecf4c00f766
jerome <jdefaver@gmail.com>
2016-06-13 11:01:56 +0200

    added tracker header digis

--- DataFormats/Phase2TrackerDigi/src/classes.h
+++ DataFormats/Phase2TrackerDigi/src/classes.h
@@ -9,4 +9,12 @@
 
 #include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerCommissioningDigi.h"
 
-#endif  // PHASE2TRACKERDIGI_CLASSES_H
+#include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerHeaderDigi.h"
+namespace {
+  struct dictionary5 {
+    edm::Wrapper<Phase2TrackerHeaderDigi > pcom0;
+    edm::Wrapper<std::vector<Phase2TrackerHeaderDigi> > pcom1;
+  };
+}
+
+#endif // PHASE2TRACKERDIGI_CLASSES_H
