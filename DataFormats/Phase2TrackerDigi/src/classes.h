0a7245510b353996a399f112300484ef3b3bbee8
jerome <jdefaver@gmail.com>
2017-07-13 13:38:19 +0200

    added data format for stubs

--- DataFormats/Phase2TrackerDigi/src/classes.h
+++ DataFormats/Phase2TrackerDigi/src/classes.h
@@ -1,11 +1,32 @@
 #ifndef PHASE2TRACKERDIGI_CLASSES_H
 #define PHASE2TRACKERDIGI_CLASSES_H
 
-#include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerDigi.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 #include "DataFormats/Common/interface/DetSetVector.h"
 #include "DataFormats/Common/interface/DetSetVectorNew.h"
+#include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerDigi.h"
 #include <vector>
+
+namespace {
+  struct dictionary {
+    edm::Wrapper<Phase2TrackerDigi> zs0;
+    edm::Wrapper< std::vector<Phase2TrackerDigi>  > zs1;
+    edm::Wrapper< edm::DetSet<Phase2TrackerDigi> > zs2;
+    edm::Wrapper< std::vector<edm::DetSet<Phase2TrackerDigi> > > zs3;
+    edm::Wrapper< edm::DetSetVector<Phase2TrackerDigi> > zs4;
+  };
+}
+
+#include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerStub.h"
+namespace {
+  struct dictionary2 {
+    edm::Wrapper<Phase2TrackerStub> zs0;
+    edm::Wrapper< std::vector<Phase2TrackerStub>  > zs1;
+    edm::Wrapper< edm::DetSet<Phase2TrackerStub> > zs2;
+    edm::Wrapper< std::vector<edm::DetSet<Phase2TrackerStub> > > zs3;
+    edm::Wrapper< edm::DetSetVector<Phase2TrackerStub> > zs4;
+  };
+}
 
 #include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerCommissioningDigi.h"
 namespace {
