b27bd9d0887db663c1196d25abc26ed01df62edf
jerome <jdefaver@gmail.com>
2017-08-29 10:10:42 +0200

    minimal change to remove byte inversion in RAW dataformat

--- EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDRawChannelUnpacker.h
+++ EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDRawChannelUnpacker.h
@@ -29,12 +29,14 @@ namespace Phase2Tracker {
   };  // end of Phase2TrackerFEDRawChannelUnpacker
 
   inline Phase2TrackerFEDRawChannelUnpacker::Phase2TrackerFEDRawChannelUnpacker(const Phase2TrackerFEDChannel& channel)
-      : data_(channel.data()),
-        currentOffset_(channel.offset()),
-        currentStrip_(0),
-        valuesLeft_((channel.length()) * 8 - STRIPS_PADDING),
-        currentWord_(channel.data()[currentOffset_ ^ 7]),
-        bitInWord_(0) {}
+    : data_(channel.data()),
+      currentOffset_(channel.offset()),
+      currentStrip_(0),
+      valuesLeft_((channel.length())*8 - STRIPS_PADDING),
+      currentWord_(channel.data()[currentOffset_]),
+      bitInWord_(0)
+  {
+  }
 
   inline Phase2TrackerFEDRawChannelUnpacker& Phase2TrackerFEDRawChannelUnpacker::operator++() {
     bitInWord_++;
@@ -42,7 +44,7 @@ namespace Phase2Tracker {
     if (bitInWord_ > 7) {
       bitInWord_ = 0;
       currentOffset_++;
-      currentWord_ = data_[currentOffset_ ^ 7];
+      currentWord_ = data_[currentOffset_];
     }
     valuesLeft_--;
     return (*this);
