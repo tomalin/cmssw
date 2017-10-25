b4251ffd1ba5b62f071fb4aeb9800e84b869f6d6
jerome <jdefaver@gmail.com>
2017-10-25 14:47:07 +0200

    split unpackers in .h and .cc

--- EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDRawChannelUnpacker.h
+++ EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDRawChannelUnpacker.h
@@ -28,33 +28,4 @@ namespace Phase2Tracker {
     uint8_t bitInWord_;
   };  // end of Phase2TrackerFEDRawChannelUnpacker
 
-  inline Phase2TrackerFEDRawChannelUnpacker::Phase2TrackerFEDRawChannelUnpacker(const Phase2TrackerFEDChannel& channel)
-    : data_(channel.data()),
-      currentOffset_(channel.offset()),
-      currentStrip_(0),
-      valuesLeft_((channel.length())*8 - STRIPS_PADDING),
-      currentWord_(channel.data()[currentOffset_]),
-      bitInWord_(0)
-  {
-  }
-
-  inline Phase2TrackerFEDRawChannelUnpacker& Phase2TrackerFEDRawChannelUnpacker::operator++() {
-    bitInWord_++;
-    currentStrip_++;
-    if (bitInWord_ > 7) {
-      bitInWord_ = 0;
-      currentOffset_++;
-      currentWord_ = data_[currentOffset_];
-    }
-    valuesLeft_--;
-    return (*this);
-  }
-
-  inline Phase2TrackerFEDRawChannelUnpacker& Phase2TrackerFEDRawChannelUnpacker::operator++(int) {
-    ++(*this);
-    return *this;
-  }
-
-}  // namespace Phase2Tracker
-
 #endif  // } end def EventFilter_Phase2TrackerRawToDigi_Phase2TrackerPhase2TrackerFEDRawChannelUnpacker_H
--- EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDZSChannelUnpacker.h
+++ EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDZSChannelUnpacker.h
@@ -40,43 +40,6 @@ namespace Phase2Tracker {
     int clustersize_;
   };
 
-  inline Phase2TrackerFEDZSChannelUnpacker::Phase2TrackerFEDZSChannelUnpacker(const Phase2TrackerFEDChannel& channel)
-    : data_(channel.data())
-  {
-      currentOffset_ = channel.offset() * 8 + channel.bitoffset();
-  }
-
-  void Phase2TrackerFEDZSChannelUnpacker::Merge()
-  {
-    clusterx_     = unMergedX();
-    clustery_     = unMergedY();
-    clustersize_  = unMergedSize();
-    while(gluedToNextCluster())
-    {
-      advance();
-      clustersize_ += unMergedSize();
-    }
-  }
-
-  inline Phase2TrackerFEDZSChannelUnpacker& Phase2TrackerFEDZSChannelUnpacker::operator ++ ()
-  {
-    currentOffset_ += clusterdatasize_; 
-    clustersLeft_--;
-    return (*this);
-  }
-
-  Phase2TrackerFEDZSChannelUnpacker& Phase2TrackerFEDZSChannelUnpacker::advance()
-  {
-    currentOffset_ += clusterdatasize_;
-    clustersLeft_--;
-    return (*this);
-  }
-
-  inline Phase2TrackerFEDZSChannelUnpacker& Phase2TrackerFEDZSChannelUnpacker::operator ++ (int)
-  {
-    ++(*this); return *this;
-  }
-
   class Phase2TrackerFEDZSSon2SChannelUnpacker : public Phase2TrackerFEDZSChannelUnpacker
   {
       public:
@@ -95,37 +58,6 @@ namespace Phase2Tracker {
           bool gluedToNextCluster() const;
           Phase2TrackerFEDZSSon2SChannelUnpacker next() const;
   };
-
-  int Phase2TrackerFEDZSSon2SChannelUnpacker::unMergedX() const
-  {
-    uint8_t id = chipId();
-    if(id>7) id -= 8;
-    return (STRIPS_PER_CBC*id + rawX())/2;
-  }
-
-  int Phase2TrackerFEDZSSon2SChannelUnpacker::unMergedY() const
-  {
-    return (chipId() > 7)?1:0;
-  }
-
-  bool Phase2TrackerFEDZSSon2SChannelUnpacker::gluedToNextCluster() const
-  {
-    uint8_t size = rawSize();
-    if (clustersLeft_ <= 1 or next().unMergedY() != unMergedY() or next().Plane() != Plane()) return false; 
-    if (next().unMergedX() == unMergedX() + size) 
-    {
-      if(size == 8 or (unMergedX() + size)%(STRIPS_PER_CBC/2) == 0) return true;
-      // else : two glued clusters split for not apparent reason 
-    }
-    return false;
-  }
-
-  Phase2TrackerFEDZSSon2SChannelUnpacker Phase2TrackerFEDZSSon2SChannelUnpacker::next() const
-  {
-    Phase2TrackerFEDZSSon2SChannelUnpacker next(*this);
-    next.advance();
-    return next;
-  }
 
   class Phase2TrackerFEDZSSonPSChannelUnpacker : public Phase2TrackerFEDZSChannelUnpacker
   {
@@ -146,36 +78,6 @@ namespace Phase2Tracker {
           Phase2TrackerFEDZSSonPSChannelUnpacker next() const;
   };
   
-  int Phase2TrackerFEDZSSonPSChannelUnpacker::unMergedX() const
-  {
-    uint8_t id = chipId();
-    if(id>7) id -= 8;
-    return PS_ROWS*id + rawX();
-  }
-
-  int Phase2TrackerFEDZSSonPSChannelUnpacker::unMergedY() const
-  {
-    return (chipId() > 7)?1:0;
-  }
-
-  Phase2TrackerFEDZSSonPSChannelUnpacker Phase2TrackerFEDZSSonPSChannelUnpacker::next() const
-  {
-    Phase2TrackerFEDZSSonPSChannelUnpacker next(*this);
-    next.advance();
-    return next;
-  }
-
-  bool Phase2TrackerFEDZSSonPSChannelUnpacker::gluedToNextCluster() const
-  {
-    uint8_t size = rawSize();
-    if (clustersLeft_ <= 1 or next().unMergedY() != unMergedY()) { return false; }
-    if (next().unMergedX() == unMergedX() + size) 
-    {
-      if(size == 8 or (unMergedX() + size)%PS_ROWS == 0) { return true; }
-    }
-    return false; 
-  }
-
   class Phase2TrackerFEDZSPonPSChannelUnpacker : public Phase2TrackerFEDZSChannelUnpacker
   {
       public:
@@ -194,36 +96,6 @@ namespace Phase2Tracker {
           bool gluedToNextCluster() const;
           Phase2TrackerFEDZSPonPSChannelUnpacker next() const;
   };
-
-  int Phase2TrackerFEDZSPonPSChannelUnpacker::unMergedX() const
-  {
-    uint8_t id = chipId();
-    if(id>7) id -= 8;
-    return PS_ROWS*id + rawX();
-  }
-
-  int Phase2TrackerFEDZSPonPSChannelUnpacker::unMergedY() const
-  {
-    return (chipId() > 7)?(rawY()+PS_COLS/2):rawY();
-  }
-
-  Phase2TrackerFEDZSPonPSChannelUnpacker Phase2TrackerFEDZSPonPSChannelUnpacker::next() const
-  {
-    Phase2TrackerFEDZSPonPSChannelUnpacker next(*this);
-    next.advance();
-    return next;
-  }
-
-  bool Phase2TrackerFEDZSPonPSChannelUnpacker::gluedToNextCluster() const
-  {
-    uint8_t size = rawSize();
-    if (clustersLeft_ <= 1 or next().unMergedY() != unMergedY()) { return false; }
-    if (next().unMergedX() == unMergedX() + size) 
-    {
-      if(size == 8 or (unMergedX() + size)%PS_ROWS == 0) return true;
-    }
-    return false; 
-  }
 
 } // end of Phase2Tracker namespace
 
