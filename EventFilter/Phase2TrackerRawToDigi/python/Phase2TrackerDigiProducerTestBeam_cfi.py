f4d681b931a1c8288e62789bd401383262c771c1
jerome <jdefaver@gmail.com>
2017-12-20 11:09:34 +0100

    removed testbeam-exclusive files

--- EventFilter/Phase2TrackerRawToDigi/python/Phase2TrackerDigiProducerTestBeam_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-Phase2TrackerDigiProducerTestBeam = cms.EDProducer(
-    'Phase2TrackerDigiProducerTestBeam',
-    ProductLabel = cms.InputTag("rawDataCollector")
-)
