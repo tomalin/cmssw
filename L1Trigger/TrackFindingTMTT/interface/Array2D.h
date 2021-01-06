600f6511a9ad9baf05ed261312eeda0fda0b3117
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2021-01-06 20:38:10 +0000

    code format

--- L1Trigger/TrackFindingTMTT/interface/Array2D.h
+++ L1Trigger/TrackFindingTMTT/interface/Array2D.h
@@ -22,14 +22,16 @@ namespace tmtt {
     Array2D(unsigned int m, unsigned int n) : array2D_(m * n), m_{m}, n_{n} {}
 
     const T& operator()(unsigned int i, unsigned int j) const {
-      if (i >= m_ || j >= n_) throw cms::Exception("LogicError")<<"Array2D: indices out of range "<<i<<" "<<j<<" "<<m_<<" "<<n_;
+      if (i >= m_ || j >= n_)
+        throw cms::Exception("LogicError")
+            << "Array2D: indices out of range " << i << " " << j << " " << m_ << " " << n_;
       return array2D_[i * n_ + j];
     }
 
     T& operator()(unsigned int i, unsigned int j) {
       // Non-const version of operator, without needing to duplicate code.
       // (Scott Meyers trick).
-      return const_cast<T&>(std::as_const(*this)(i,j));
+      return const_cast<T&>(std::as_const(*this)(i, j));
     }
 
   private:
